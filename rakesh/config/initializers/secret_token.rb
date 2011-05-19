7f230030ddd2513d83226a2891bba73abb76acbf
lokesh1 <chowdary_rakesh2003@yahoo.com>
2011-05-19 11:05:33 +0530

    initial commit

---
+++ rakesh/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rakesh::Application.config.secret_token = '296e93e5b0563da6a285a237f4931f9f16ba4284fb7a681b6b4afa61abbb91667324d867625200cfdb259c5bb428996ba5b828d69ced551aea5b29cf8cb07aa9'
